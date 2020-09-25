3a61c9cdd610403605351ab7cdd983efab98a29e
River Moreira <river.moreira-ferreira@ens.uvsq.fr>
2020-09-25 03:37:58 -0400

    td1:Program Simple Fraction
    Main.java:File main of the java program.
    Fraction.java:Implementation of Fraction class

---
+++ Fraction.java
@@ -0,0 +1,12 @@
+public class Fraction{
+	int numerateur;
+	int denominateur;
+	
+	Fraction(int numerateur,int denominateur){
+		this.numerateur=numerateur;
+		this.denominateur=denominateur;
+		}
+	
+	
+	
+	}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+	
+	public static void main(String args[]){
+		Fraction test=new Fraction(14,3);
+		System.out.println("Hello Wolrd!");
+		
+		
+		}
+	
+	
+	}
